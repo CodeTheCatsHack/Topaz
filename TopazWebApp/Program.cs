509a0450bc6a546d0733d679bbe492bf7554da4f
dmitr <dmitriy.kolykhalov@mail.ru>
2023-11-10 17:52:56 +0300

    Редактирование потока загрузки веб-интерфейса TopazWebApp, удаление лишних Blazor компонентов, подключение библиотеки Blazorice компонентов.

--- TopazWebApp/Program.cs
+++ TopazWebApp/Program.cs
@@ -1,29 +1,58 @@
-using Topaz.Data;
+using Blazorise;
+using Blazorise.Bootstrap5;
+using Blazorise.Icons.FontAwesome;
+using Scaffold;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+try
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    var builder = WebApplication.CreateBuilder(args);
+
+    Log.Information("Загрузка Core приложения...");
+    builder.Services.AddCoreDi(builder.Configuration);
+
+    Log.Information("Загрузка анализаторов Blazor Server...");
+    builder.Services.AddRazorPages();
+    builder.Services.AddServerSideBlazor();
+    builder.Services
+        .AddBlazorise(options => { options.Immediate = true; })
+        .AddBootstrap5Providers()
+        .AddFontAwesomeIcons()
+        .AddBootstrap5Components();
+
+    builder.Services.AddHttpContextAccessor();
+
+    var app = builder.Build();
+
+    Log.Information("Разрешение использования кросс-доменных запросов...");
+    app.UseCors(x => x
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .SetIsOriginAllowed(_ => true)
+        .AllowCredentials());
+
+    app.UseExceptionHandler("/error");
+
+    Log.Information("Настройка Redirect HTTP на HTTPS...");
+    app.UseHsts()
+        .UseHttpsRedirection();
+
+    Log.Information("Надстройка статических файлов...");
+    app.UseDefaultFiles()
+        .UseStaticFiles();
+
+    Log.Information("Настройка маршрутизации по компонентам Blazor...");
+    app.MapBlazorHub();
+    app.MapFallbackToPage("/_Host");
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
-app.Run();
+catch (Exception ex) when (ex.GetType().Name is not "StopTheHostException" and "HostAbortedException")
+{
+    Log.Fatal(ex, $"Ошибка:{ex.Message}, Доп. информация по ошибке:${ex.InnerException?.Message ?? "Нет информации!"}");
+}
+finally
+{
+    Log.Information("Закрытие веб приложения...");
+    Log.CloseAndFlush();
+}
