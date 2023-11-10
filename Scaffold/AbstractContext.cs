1ecad3b4487fc61c05d237a757eed1e8bd12c69b
dmitr <dmitriy.kolykhalov@mail.ru>
2023-11-10 18:54:38 +0300

    Подтягиваем Ядро Asp.Net и абстракции с частичными классами моделей EF Core.

---
+++ Scaffold/AbstractContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Scaffold;
+
+public abstract class AbstractContext<T> : DbContext
+    where T : DbContext
+{
+    public AbstractContext(DbContextOptions<T> options) : base(options)
+    {
+    }
+
+    public IQueryable<string> SelectExpression(string nameFunction, List<string> parametrList)
+    {
+        return Database.SqlQuery<string>(
+            $"SELECT `{nameFunction}`({parametrList.Aggregate((x, y) => $"\"{x}\",\"{y}\"")});");
+    }
+
+    public IQueryable<string> SelectExpression(string nameFunction, string parametr)
+    {
+        return Database.SqlQuery<string>($"SELECT `{nameFunction}`(\"{parametr}\");");
+    }
+}
---
+++ Scaffold/CoreDiConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Compact;
+
+namespace Scaffold;
+
+public static class CoreDiConfiguration
+{
+    public static readonly Action<HostBuilderContext, LoggerConfiguration> ConfigurationSerilog = (_, configuration) =>
+        configuration.MinimumLevel.Debug()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
+            .WriteTo.File(new CompactJsonFormatter(), "log.json");
+}
