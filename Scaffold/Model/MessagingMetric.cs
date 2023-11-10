ae7e49ccc9ff28f07e2f972a4b48f447cf9d46a3
sokolov <sokolov.ispp01@gmail.com>
2023-11-10 15:27:45 +0300

    scaffold project added

---
+++ Scaffold/Model/MessagingMetric.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Scaffold.Model;
+
+[Index("MeasureId", Name = "fk_MessagingMetric_Measure1_idx", IsUnique = true)]
+public partial class MessagingMetric
+{
+    /// <summary>
+    /// Идентификатор качества услуг подвижной радиотелефонной связи в части передачи коротких текстовых сообщений
+    /// </summary>
+    [Key]
+    public int id { get; set; }
+
+    /// <summary>
+    /// Внешний идентификатор измерения
+    /// </summary>
+    public int MeasureId { get; set; }
+
+    /// <summary>
+    /// Доля недоставленных SMS сообщений [%]
+    /// </summary>
+    public float UndeliveredMessagePercentage { get; set; }
+
+    /// <summary>
+    /// Среднее время доставки SMS сообщений [сек]
+    /// </summary>
+    public float AverageMessageDeliveryTime { get; set; }
+
+    [ForeignKey("MeasureId")]
+    [InverseProperty("MessagingMetric")]
+    public virtual Measure Measure { get; set; } = null!;
+}
